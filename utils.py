3f8aedefd298fb9e5e6b60da3be794ff8ff1698e
Tom Lee <tom.kennedy.lee@gmail.com>
2020-05-27 01:23:23 -0400

    new heroku test

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def get_header(app):
                         className="logo",
                     ),
                     html.A(
-                        html.Button("REPOSITORY", id="learn-more-button"),
+                        html.Button("1", id="learn-more-button"),
                         href="https://github.com/tl212/duo_esse",
                     ),
                 ],
